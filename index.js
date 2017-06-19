0f15e4561a3d38ea51889d8c47e5ecf74cc7244c
Devansh <malhotra.devansh@gmail.com>
2017-06-20 02:45:54 +0530

    Updated js file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var topPackages = new Set();
+var packageMap = new Object();
 function search(s)
 {
 		var output = document.getElementById('gitList');
@@ -6,7 +7,7 @@ function search(s)
       		'message' : s.value,
       		'published':false,
       	};
-		var url='https://api.github.com/search/repositories?q='+s.value+'+language:assembly&sort=stars&order=desc';
+		var url='https://api.github.com/search/repositories?q='+s.value;
 		$.ajax({
 		url: url,
 		type:"GET",
@@ -14,25 +15,92 @@ function search(s)
 		contentType:"application/json",
 		success: function(data) {
 			console.log(data['items']);
+            var body = document.getElementsByTagName('body')[0];
+            body.removeChild(body.lastChild); // Remove last created serach result
+
+            var mainBody = document.createElement('div');
+
+            var tbl = document.createElement('table');
+            tbl.style.width = '100%';
+            tbl.setAttribute('border', '1');
+
+            // Creating bar chart axis
+            var x = new Array(10);
+            var y = new Array(10);
+
+            // Creting table header
+            var header = tbl.createTHead();
+            var hrow = header.insertRow(0);
+            var hcell1 = hrow.insertCell(0);
+            hcell1.innerHTML = "<b>Repository Name</b>";
+            var hcell2 = hrow.insertCell(1);
+            hcell2.innerHTML = "<b>Fork Count</b>";
+            var hcell3 = hrow.insertCell(2);
+            hcell3.innerHTML = "<b>Star Count</b>";
+            var hcell4 = hrow.insertCell(3);
+            hcell4.innerHTML = "<b>Import packages from this repository</b>";
+
+            // Generating table rows
+            var tbdy = document.createElement('tbody');
 			for(var i=0;i<data['items'].length;i++)
     		{
-    			var gitRepo = document.createElement("p");
+                var tr = document.createElement('tr');
+                var td1 = document.createElement('td');
+                td1.appendChild(document.createTextNode(data['items'][i].full_name));
+                var td2 = document.createElement('td');
+                td2.appendChild(document.createTextNode(data['items'][i].forks_count));
+                var td3 = document.createElement('td');
+                td3.appendChild(document.createTextNode(data['items'][i].stargazers_count));
+                var td4 = document.createElement('td');
+
+                var contents_url = data['items'][i].contents_url;
+
+                if(i < 10)
+                {
+                    x[i] = data['items'][i].full_name;
+                    y[i] = data['items'][i].stargazers_count;
+                }
+    			/*var gitRepo = document.createElement("p");
     			var contents_url = data['items'][i].contents_url;
-    			gitRepo.innerHTML = data['items'][i].name + " " + data['items'][i].forks + " " + data['items'][i].stargazers_count 
+    			gitRepo.innerHTML = data['items'][i].full_name + " " + data['items'][i].forks_count + " " + data['items'][i].stargazers_count
     				+ " " + data['items'][i].contents_url;
     				// + " " + '<button id="but' + i +'" onclick="clickImport(contents_url)"> Import </button>';
-    			
+    			*/
     			var btn = document.createElement("button");
-    			btn.innerHTML = 'Import ' + i;
-    			btn.setAttribute("id", 'btn'+i)
+    			btn.innerHTML = 'Import';
+    			btn.value = contents_url;
+    			btn.setAttribute("id", 'btn'+i);
 				btn.addEventListener("click", function(event){
-    			clickImport(contents_url);
+    			clickImport(btn.value);
     			event.preventDefault();
     		});
-    			output.appendChild(gitRepo);
-				output.appendChild(btn);
+                td4.appendChild(btn);
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3);
+                tr.appendChild(td4);
+    			/*output.appendChild(gitRepo);
+				output.appendChild(btn);*/
+                tbdy.appendChild(tr);
     		}
-    		document.body.appendChild(output);
+            tbl.appendChild(tbdy);
+			mainBody.appendChild(tbl);
+            // body.appendChild(tbl);
+            var data = [
+                {
+                    x: x,
+                    y: y,
+                    type: 'bar'
+                }
+            ];
+
+            var graph = document.createElement("div");
+            Plotly.newPlot(graph, data);
+            graph.setAttribute("align","center");
+            mainBody.appendChild(graph);
+            //body.appendChild(graph);
+            body.appendChild(mainBody);
+    		//document.body.appendChild(output);
 		},
 		error: function(data, e1, e2) {
 			alert(" message error")
@@ -43,7 +111,7 @@ function search(s)
 
 function clickImport(url)
 {
-	// console.log("clicked with url: " + url);
+	console.log("clicked with url: " + url);
 	url=url.substring(0,url.length-7);
 	$.ajax({
 		url: url,
@@ -55,7 +123,7 @@ function clickImport(url)
 			var flag=false;
 			for(var i=0;i<list;i++)
 			{
-				if(data[i].name===".gitignore")
+				if(data[i].name==="package.json")
 				{
 
 					flag=true;
@@ -69,7 +137,6 @@ function clickImport(url)
 		},
 		error: function(data, e1, e2) {
 		console.log(" message error")
-
 		}
 	})
 }
@@ -79,15 +146,40 @@ function getDependencies(url)
 	$.ajax({
 		url: url,
 		type:"GET",
+        contentType:"application/json",
 		success: function(data) {
-			data = data.split("\n");
-			console.log(data.length);
-			for(var i=0;i<data.length;i++)
+			// data = data.split("\n");
+            var dep = data.dependencies;
+            var depSize = dep.length;
+            console.log(depSize);
+            var devDep = data.devDependencies;
+            var devDepSize = devDep.length;
+			console.log(devDepSize);
+			/*for(var i=0;i<depSize;i++)
 			{
 				console.log(i + ": " + data[i]);
 				topPackages.add(data[i]);
-			}
-			helper(topPackages);
+			}*/
+			for(var name in dep)
+            {
+                if(name in packageMap) {
+                    packageMap[name] = packageMap[name] + 1;
+                }
+                else{
+                    packageMap[name] = 1;
+                }
+            }
+            for(var name in devDep)
+            {
+                if(name in packageMap) {
+                    packageMap[name] = packageMap[name] + 1;
+                }
+                else{
+                    packageMap[name] = 1;
+                }
+            }
+			// helper(topPackages);
+            console.log(packageMap);
 		},
 		error: function(data, e1, e2) {
 		console.log(" message error")
